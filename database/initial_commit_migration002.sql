8faeacdb000492722a5b4c3118858481dcc88470
Aaron Anderson <aaron.andersonm@gmail.com>
2023-02-28 13:46:28 -0700

    aaron-commit-final final version of the migration checks

--- database/initial_commit_migration002.sql
+++ database/initial_commit_migration002.sql
@@ -1,22 +1,11 @@
 -- A username should be at least 4 characters long
 ALTER User ADD CHECK (Username>=4);
 
--- An email address must follow the standards for a valid email address
-
 -- Email addresses must not be on the DNS block list (CIS 9.2)
-CHECK (email REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
-
--- Passwords must be hashed and salted before storing. They must be at least 12 characters long.
-
--- Bitcoin wallet must start with 0x and only contain numbers afterwards
+ALTER User ADD CHECK (email REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
 
 -- IP must follow the standard X.X.X.X where X is between 0 and 256
-CHECK (ip REGEXP '^([0-9]{1,3}\.){3}[0-9]{1,3}$')
-
--- All IDs (Primary Keys) are defined using UUID format (XXXXXXXX-XXXX-4XXX-XXXX-XXXXXXXXXXXX)
-CHECK (User_ID REGEXP '^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-4[0-9A-Fa-f]{3}-[8|9|A|a|B|b][0-9A-Fa-f]{3}-[0-9A-Fa-f]{12}$')
-
--- Botnet order price is a calculated field, equal to number of bots * bot cost
+ALTER Bots ADD CHECK (IP_Address REGEXP '^([0-9]{1,3}\.){3}[0-9]{1,3}$');
 
 -- Time_of_use (in months) cannot be longer than 12 months
 ALTER Botnet_Order ADD CHECK (Time_of_use<=12 AND Time_of_use>=0);
